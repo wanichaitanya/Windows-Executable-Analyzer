4ed6b6fcbb76afb5515384f6041a33fc64b6b8dd
wanichaitanya <35448900+wanichaitanya@users.noreply.github.com>
2018-05-03 16:18:32 +0530

    Header.h Created

---
+++ Header/Header.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include<stdio.h>
+#include<iostream>
+#include<Windows.h>
+#include<iomanip>
+#include<stdlib.h>
+#include<io.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<conio.h>
+#include<iostream>
+using namespace std;
+
+#endif /*HEADER_H*/
